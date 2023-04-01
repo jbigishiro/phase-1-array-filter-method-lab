fd2192ad8606710da4166aa2ea20746b8dd778de
Justin Bigishiro <justin.rvd@gmail.com>
2023-04-01 16:45:25 -0500

    adds myfile

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter(function(driverName){
+        return driverName.toLowerCase()===name.toLowerCase()
+    });
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function (driverName){
+        return driverName.slice(0, letters.length)===letters
+    } )
+}
+
+function matchName(drivers, name){
+    return drivers.filter( function(driver){
+        return driver.name===name
+    })
+}
